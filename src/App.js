70ab9a2f97a329c6f173e5e7ce748d50bdd65847
Colten Yeats <coltenyeats@gmail.com>
2024-04-10 14:37:19 -0400

    i just wanna see what the scoreboard looks like dude

--- src/App.js
+++ src/App.js
@@ -46,6 +46,23 @@ return (
   <Link to="/">
     Click here to return to scoreboard view
   </Link>
+
+  <div className="scoreboard_page">
+        <div className="scoreboard_container">
+            <div className="team_one_score">
+                <h2>Team 1</h2>
+                <h1>20</h1>
+            </div>
+            <div className="question_counter">
+                <h3>Question 3</h3>
+            </div>
+            <div className="team_two_score">
+                <h2>Team 2</h2>
+                <h1>20</h1>
+            </div>
+        </div>
+        <Link to="/">Click here to enter scorecard mode!</Link>
+    </div>
 </div>
 );
 }
